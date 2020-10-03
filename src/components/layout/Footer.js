38a9274e28f42d3d35e27aed773a5395267b15d5
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-03 12:14:36 -0400

    more content injection and refinement

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,17 +4,20 @@ const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">About Us</h2>
-        <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
+        <h2 className="text-lg font-semibold">About Hayden</h2>
+        <p className="mt-5">After 5 years in professional aviation I now offer web presence and branding services.</p>
       </div>
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
+        <h2 className="text-lg font-semibold">Contact Info</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://codebushi.com">Terms &amp; Conditions</a>
+            <a href="mailto:haydenyounglex@hotmail.com">Email</a>
           </li>
           <li>
-            <a href="https://codebushi.com">Privacy Policy</a>
+            <a href="tel:+18596840445">+1 (859) 684-0445</a>
+          </li>
+          <li>
+            <a href="https://haydenyoung.me">haydenyoung.me</a>
           </li>
         </ul>
       </div>
@@ -22,13 +25,13 @@ const Footer = () => (
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://dev.to/changoman">Dev.to</a>
+            <a href="https://www.linkedin.com/in/hayden-young-atp/">LinkedIn</a>
           </li>
           <li>
-            <a href="https://twitter.com/HuntaroSan">Twitter</a>
+            <a href="https://twitter.com/haydenry">Twitter</a>
           </li>
           <li>
-            <a href="https://github.com/codebushi/gatsby-starter-lander">GitHub</a>
+            <a href="https://github.com/ztlhayden">GitHub</a>
           </li>
         </ul>
       </div>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import LogoIcon from '../../svg/LogoIcon';
+import LogoIcon from '../../assets/icon/dd_icon.svg';
 import Button from '../Button';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
-        <div className="w-12 mr-3">
-          <LogoIcon />
+        <div className="w-12 mr-4">
+          <img src={LogoIcon} atl="" />
         </div>
-        Lander
+        Deadhead.Design
       </div>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#features">
@@ -19,15 +19,15 @@ const Header = () => (
         <AnchorLink className="px-4" href="#services">
           Services
         </AnchorLink>
-        <AnchorLink className="px-4" href="#stats">
-          Stats
+        <AnchorLink className="px-4" href="/about">
+          About
         </AnchorLink>
-        <AnchorLink className="px-4" href="#testimonials">
-          Testimonials
+        <AnchorLink className="px-4" href="/portfolio">
+          Portfolio
         </AnchorLink>
       </div>
       <div className="hidden md:block">
-        <Button className="text-sm">Start Free Trial</Button>
+        <Button className="text-sm">Free Quote</Button>
       </div>
     </div>
   </header>

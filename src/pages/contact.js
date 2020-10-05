28682b9a6d1d00155445bf9f07f283b745227ad4
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-05 17:47:43 -0400

    init contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout/Layout'
+
+export default () => (
+    <Layout>
+    <div className='container'>
+      <form action="post">
+        
+      </form>
+
+    </div>
+    </Layout>
+)

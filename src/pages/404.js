1332406b817d7995eaef6dca2c4ce12e10432a99
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-05 13:49:21 -0400

    custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/layout/Layout'
+
+export default () => (
+  <Layout>
+    <div>
+      <h1>Page not Found</h1>
+    </div>
+  </Layout>
+)

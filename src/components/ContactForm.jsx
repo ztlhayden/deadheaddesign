8d051507dbf58598e58de8251728d430857c22fc
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-06 11:31:02 -0400

    basic form function

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const ContactForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
-    <form action="post" className="w-full">
+    <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
       <h2 className="text-center font-semibold text-lg">Request an Email</h2>
       <div>
         <label htmlFor="name" className="block mb-1">
@@ -12,10 +16,11 @@ const ContactForm = () => {
           type="text"
           name="name"
           id="name"
-          className="w-full rounded"
+          className="w-full rounded text-contrast_dark"
           placeHolder="Your Name"
+          ref={register({ required: true })}
         />
-        <p className="text-red font-thin italic">Please include your name.</p>
+        {errors.name && <p className="text-red font-thin italic">Please include your name.</p>}
       </div>
       <div>
         <label htmlFor="email" className="block mt-4 mb-1">
@@ -25,10 +30,11 @@ const ContactForm = () => {
           type="email"
           name="email"
           id="email"
-          className="w-full rounded"
+          className="w-full rounded text-contrast_dark"
           placeHolder="Your Email"
+          ref={register({ required: true })}
         />
-        <p className="text-red font-thin italic">Please check your email.</p>
+        {errors.email && <p className="text-red font-thin italic">Please check your email.</p>}
       </div>
       <div>
         <label htmlFor="body" className="block mt-4 mb-1">
@@ -39,8 +45,9 @@ const ContactForm = () => {
           id="body"
           cols="24"
           rows="5"
-          className="w-full rounded"
+          className="w-full rounded text-contrast_dark"
           placeHolder="Your Amazing Idea"
+          ref={register}
         />
       </div>
       <button

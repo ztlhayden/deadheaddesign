59c5cc57843a7f3df6023fe2229b9af92b3195ce
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-06 10:56:41 -0400

    style contact page

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const ContactForm = () => {
+  return (
+    <form action="post" className="w-full">
+      <h2 className="text-center font-semibold text-lg">Request an Email</h2>
+      <div>
+        <label htmlFor="name" className="block mb-1">
+          Name
+        </label>
+        <input type="text" name="name" id="name" className="w-full" placeHolder="Your Name" />
+        <p className="text-red font-thin italic">Please include your name.</p>
+      </div>
+      <div>
+        <label htmlFor="email" className="block mt-4 mb-1">
+          Email
+        </label>
+        <input type="email" name="email" id="email" className="w-full" placeHolder="Your Email" />
+        <p className="text-red font-thin italic">Please check your email.</p>
+      </div>
+      <div>
+        <label htmlFor="body" className="block mt-4 mb-1">
+          What's your project?
+        </label>
+        <textarea
+          name="body"
+          id="body"
+          cols="24"
+          rows="5"
+          className="w-full"
+          placeHolder="Your Amazing Idea"
+        />
+      </div>
+      <button type="submit" className="mt-2 bg-white text-contrast_dark font-semibold p-1 w-2/6">
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,46 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/layout/Layout'
+import Layout from '../components/layout/Layout';
 
-import PhoneIcon from '../assets/icon/phone.svg'
-import ComputerIcon from '../assets/icon/computer.svg'
+import PhoneIcon from '../assets/icon/phone.svg';
+import ComputerIcon from '../assets/icon/computer.svg';
+import ContactForm from '../components/ContactForm';
 
 export default () => (
-    <Layout>  
-    <h1 className="text-3xl text-center m-10 font-semibold">Drop us a line!</h1>
-    <div className="container m-auto flex flex-wrap justify-around">
-      <section className="max-w-sm">
-        <p>Free Quotes</p>
-      </section>
-      <form action="post" className="max-w-sm">
-        <div>
-          <label htmlFor="name" className="block mt-4 mb-1">Name:</label>
-          <input type="text" name="name" id="name" className="w-full"/>
+  <Layout>
+    <div className="container m-auto">
+      <h1 className="text-3xl text-center m-5 font-semibold">Drop us a line!</h1>
+      <div className="min-w-100 m-auto flex flex-wrap justify-between">
+        <section className="max-w-sm w-full text-center ml-auto mr-auto">
+          <h2 className="font-semibold text-lg m-5">Free Quotes</h2>
+          <p>
+            Your quote will include a personal consultation and a design mock-up, all free of charge
+            and with no obligation. However, I am confident you will love it, and look forward to
+            working with you.
+          </p>
+        </section>
+
+        <div className="max-w-sm w-full p-3 ml-auto mr-auto bg-contrast_dark text-white">
+          <ContactForm />
         </div>
-        <div>
-          <label htmlFor="email" className="block mt-4 mb-1">Email:</label>
-          <input type="email" name="email" id="email" className="w-full"/>
-        </div>
-        <div>
-          <label htmlFor="phone" className="block mt-4 mb-1">Phone:</label>
-          <input type="tel" name="phone" id="phone" className="w-full"/>
-        </div>
-        <div>
-          <label htmlFor="body" className="block mt-4 mb-1">What's on your mind?</label>
-          <textarea name="body" id="body" cols="24" rows="5" className="w-full"/>
-        </div>
-          <button type="submit">Send</button>
-      </form>
-      <section className="max-w-sm">
-        <a className="block" href="mailto:hayden@haydenyoung.me">
-          <img src={ComputerIcon} alt="" width="50"/>
-          hayden@haydenyoung.me
-        </a>
-        <a className="block" href="tel:+18596840445">
-          <img src={PhoneIcon} alt="" width="50"/>
-          (859) 684-0445
-        </a>
-      </section>
+
+        <section className="max-w-sm w-full text-center ml-auto mr-auto">
+          <h2 className="font-semibold text-lg m-5">Reach Out</h2>
+          <div className="">
+            <a className="block mb-3 m-auto" href="mailto:hayden@haydenyoung.me">
+              <img src={ComputerIcon} alt="" width="30" className="float-left mr-2" />
+              <span className="underline">hayden@haydenyoung.me</span>
+            </a>
+            <a className="block m-auto" href="tel:+18596840445">
+              <img src={PhoneIcon} alt="" width="30" className="float-left mr-2" />
+              <span className="underline">(859) 684-0445</span>
+            </a>
+          </div>
+        </section>
+      </div>
     </div>
-    </Layout>
-)
+  </Layout>
+);

84beb90e8a8756b22845c2f63b399e911e073b4d
Hayden Young <haydenyounglex@ohtmail.com>
2020-11-04 14:07:49 -0500

    background color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: 'RGB(245, 245, 255)',
+        white: 'RGB(250, 250, 255)',
         bright_white: 'RGB(255, 255, 255)',
         green: 'RGB(60, 203, 79)',
         red: 'RGB(255, 40, 35)',
         contrast_light: 'RGB(255, 111, 43)',
         contrast_light_darker: 'RGB(235, 91, 23)',
-        contrast_dark: 'RGB(31, 36, 90)', //#1F245A
+        contrast_dark: 'RGB(31, 36, 90)', // #1F245A
         contrast_dark_lighter: 'RGB(201, 206, 255)',
         primary: 'RGB(1, 1, 2)',
         black: 'RGB(1, 2, 3)'
@@ -17,4 +17,4 @@ module.exports = {
   },
   variants: {},
   plugins: []
-};
+}

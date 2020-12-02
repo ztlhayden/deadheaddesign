09465931d1ac2ad131ae740c402a12f3cd5e0d4a
Hayden Young <haydenyounglex@ohtmail.com>
2020-12-02 17:34:21 -0500

    updates

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import GlobalStyle from './GlobalStyles/GlobalStyle'
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
@@ -12,6 +14,10 @@ const Layout = ({ children }) => {
       <Footer />
     </body>
   )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
